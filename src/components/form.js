1073b39325aa46a7c56395906cb19024bec65682
CesarRivasP <cesarjose58@gmail.com>
2019-03-02 18:49:38 -0400

    added form component

---
+++ src/components/form.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Form extends Component {
+
+    render() {
+        return (
+            <div>
+                content
+            </div>
+        );
+    }
+}
+
+export default Form;
