fb15161e6e864ab77717fddd7d058d33b289daed
CesarRivasP <cesarjose58@gmail.com>
2019-03-02 19:15:15 -0400

    Added inputs, labels and others components

--- src/components/form.js
+++ src/components/form.js
@@ -4,9 +4,34 @@ class Form extends Component {
 
     render() {
         return (
-            <div>
-                content
-            </div>
+            <form>
+              <div>
+                <label>Cantidad Prestamo</label>
+                <input 
+                  className="u-full-width"
+                  type="number" 
+                  name="cantidad" 
+                  placeholder="Ejemplo: 3000" 
+                />
+              </div>  
+              <div>
+                <label>Plazo para pagar</label>
+                <select 
+                  className="u-full-width" 
+                  name="plazo" 
+                  placeholder="Ejemplo: 3000" >
+                    <option value="">Seleccionar</option>
+                    <option value="3">3 meses</option>
+                    <option value="6">6 meses</option>
+                    <option value="12">12 meses</option>
+                    <option value="24">24 meses</option>
+                    <option value="36">36 meses</option>
+                </select>
+              </div> 
+              <div>
+                <input className="u-full-width button-primary" type="submit" value="calculate" />
+              </div>
+            </form>
         );
     }
 }
