db53bc5d2e9c8c1d1dd06ed0db19bde018f551f7
CesarRivasP <cesarjose58@gmail.com>
2019-03-03 08:53:02 -0400

    Added helpers

---
+++ src/helpers.js
@@ -0,0 +1,16 @@
+export const calculateTotal = (quantity, term) => {
+    console.log('Desde helpers');
+    
+    //  Cantidades 
+    // De 0 a 1000 = 25%
+    // De 1001 a 5000 = 20%
+    // De 5001 a 10000 = 25%
+    // + 10001 = 10%
+
+    //Meses
+    // 3 = 5%
+    // 6 = 10%
+    // 12 = 15%
+    // 24 = 20%
+    // 36 = 25%
+}
