628b4849d6fd9775484173f49c7c0eb2c3e512ac
CesarRivasP <cesarjose58@gmail.com>
2019-03-03 10:47:39 -0400

    Added data result

--- src/components/result.js
+++ src/components/result.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Result = () => {
+const Result = (props) => {
+  const { quantity, term, total } = props;
   return (
     <div className="u-full-width result">
       <h2>Resultados</h2>
+      <p>La cantidad solicitada fue ${quantity}</p>
+      <p>A pagar en: {term} meses</p>
+      <p>Total a pagar: ${total}</p>
+      <p>Su pago mensual es de: ${ (total / term).toFixed(2) }</p>
     </div>
   );
 }
