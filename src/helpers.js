b3c35c9259106d120b435edfa6e697ce9e200fba
CesarRivasP <cesarjose58@gmail.com>
2019-03-03 09:04:26 -0400

    Added cases for interest calculate

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,28 @@
 export const calculateTotal = (quantity, term) => {
     console.log('Desde helpers');
     
-    //  Cantidades 
-    // De 0 a 1000 = 25%
-    // De 1001 a 5000 = 20%
-    // De 5001 a 10000 = 25%
-    // + 10001 = 10%
+    let totalQuantity;
+    
+    //  Cantidades
+    if(quantity <= 1000){
+      // De 0 a 1000 = 25%
+      totalQuantity = quantity * .25;
+    }
+    else if(quantity > 1000 && quantity <= 5000){
+      // De 1001 a 5000 = 20%
+      totalQuantity = quantity * .20;
+    }
+    else if(quantity > 5000 && quantity <= 10000){
+      // De 5001 a 10000 = 15%
+      totalQuantity = quantity * .15;
+    }
+    else {
+      // + 10001 = 10%
+      totalQuantity = quantity * .10;
+    }
 
+    console.log(totalQuantity);
+    
     //Meses
     // 3 = 5%
     // 6 = 10%
