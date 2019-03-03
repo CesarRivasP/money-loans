fa1d9b7d5e08a3cf837d9e13835349cf774647f7
CesarRivasP <cesarjose58@gmail.com>
2019-03-03 12:13:40 -0400

    Added Render spinner conditionals

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component/*, Fragment*/ } from 'react';
 import Form from './components/form';
 import Result from './components/result';
 import Message from './components/message';
+import Spinner from './components/spinner';
 import { calculateTotal } from './helpers';
 import './normalize.css';
 import './skeleton.css';
@@ -11,7 +12,8 @@ class App extends Component {
   state = {
     total: '',
     quantity: '',
-    term: ''
+    term: '',
+    loading: false
   }
 
   dataLoans = (quantity, term) => {
@@ -20,21 +22,33 @@ class App extends Component {
     const total = calculateTotal(quantity, term);
 
     this.setState({
-      total,
-      quantity,
-      term
+      loading: true
+      }, 
+      () => { //calback, esta funcion se ejecutara despues de que el state haya cambiado
+        setTimeout(() => {
+          this.setState({
+            total,
+            quantity,
+            term,
+            loading: false
+          })
+        }, 3000);
     })
   }
 
-  renderResult = (quantity, term, total) => {
+  renderResult = (quantity, term, total, loading) => {
+    const RenderOption = loading === true ? Spinner : Message;
     return (
-      !total ? <Message /> :  <Result quantity={quantity} term={term} total={total} />
+      !total || loading === true ? 
+        <RenderOption /> 
+        :  
+        <Result quantity={quantity} term={term} total={total} />
     )
   }
 
   render() {
 
-    const { quantity, term, total } = this.state;
+    const { quantity, term, total, loading } = this.state;
 
     return (
       <div>
@@ -43,7 +57,7 @@ class App extends Component {
           <Form dataLoans={this.dataLoans} />
           <div className="messages">
             {
-              this.renderResult(quantity, term, total)
+              this.renderResult(quantity, term, total, loading)
             }
           </div>
         </div>
